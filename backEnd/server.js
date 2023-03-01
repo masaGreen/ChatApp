7254a52f0972b08cb8bfcc761a03746ad5975a86
Masaa <davidmachariamj@gmail.com>
2023-03-01 08:04:09 +0300

    fixing socket

--- backEnd/server.js
+++ backEnd/server.js
@@ -40,45 +40,45 @@ app.listen(3500, () => {
   console.log("server running at post 3500");
 });
 
-const socketio = new Server(3800, {
-  cors: {
-    origin: "https://chat-app-client-pi-mauve.vercel.app",
-  }
-});
+// const socketio = new Server(3800, {
+//   cors: {
+//     origin: "https://chat-app-client-pi-mauve.vercel.app",
+//   }
+// });
 
-let users = [];
-function checkUser(userId, socketId) {
-  const newUser = users.find((user) => user.userId === userId);
-  if (newUser) return;
-  users.push({ socketId, userId });
-}
+// let users = [];
+// function checkUser(userId, socketId) {
+//   const newUser = users.find((user) => user.userId === userId);
+//   if (newUser) return;
+//   users.push({ socketId, userId });
+// }
 
-function findUser(id) {
-  const socketUser = users.find((user) => user.userId === id);
-  return socketUser;
-}
-function removeUser(id) {
-  users = users.filter((user) => user.socketId !== id);
-  return users;
-}
+// function findUser(id) {
+//   const socketUser = users.find((user) => user.userId === id);
+//   return socketUser;
+// }
+// function removeUser(id) {
+//   users = users.filter((user) => user.socketId !== id);
+//   return users;
+// }
 
-socketio.on("connection", (socket) => {
-  socket.on("addUser", (user) => {
-    checkUser(user, socket.id);
+// socketio.on("connection", (socket) => {
+//   socket.on("addUser", (user) => {
+//     checkUser(user, socket.id);
 
-    socketio.emit("getUsers", users);
-  });
+//     socketio.emit("getUsers", users);
+//   });
 
-  socket.on("message", (data) => {
-    const myUser = findUser(data.members[1]);
+//   socket.on("message", (data) => {
+//     const myUser = findUser(data.members[1]);
 
-    if (myUser) {
-      socketio.to(myUser.socketId).emit("newmess", data);
-    }
-  });
+//     if (myUser) {
+//       socketio.to(myUser.socketId).emit("newmess", data);
+//     }
+//   });
 
-  socket.on("disconnect", () => {
-    removeUser(socket.id);
-    socketio.emit("leftUsers", users);
-  });
-});
+//   socket.on("disconnect", () => {
+//     removeUser(socket.id);
+//     socketio.emit("leftUsers", users);
+//   });
+// });
--- socketIo/index.js
+++ socketIo/index.js
@@ -1,53 +1,42 @@
+const io = require("socket.io");
 
-// const io = require("socket.io")
+const socketio = io(3800, {
+  cors: {
+    origin: "http://localhost:3000",
+  },
+});
+let users = [];
+function checkUser(userId, socketId) {
+  const newUser = users.find((user) => user.userId === userId);
+  if (newUser) return;
+  users.push({ socketId, userId });
+}
 
+function findUser(id) {
+  const socketUser = users.find((user) => user.userId === id);
+  return socketUser;
+}
+function removeUser(id) {
+  users = users.filter((user) => user.socketId !== id);
+  return users;
+}
 
+socketio.on("connection", (socket) => {
+  socket.on("addUser", (user) => {
+    checkUser(user, socket.id);
+    socketio.emit("getUsers", users);
+  });
 
-// const socketio = io(3800,{
-//     cors:{
-//         origin:"http://localhost:3000"
-//     }
-// })
-// let users = []
-// function checkUser(userId,socketId){
-//     const newUser = users.find(user=>user.userId === userId)
-//     if(newUser) return
-//     users.push({socketId, userId})
-//    }
+  socket.on("message", (data) => {
+    const myUser = findUser(data.members[1]);
 
-// function findUser(id){
-//     const socketUser = users.find(user=>user.userId === id)
-//     return socketUser
-// }
-// function removeUser(id){
-//     users =  users.filter(user=>user.socketId !== id)
-//     // return newUsers
-// }
+    if (myUser) {
+      socketio.to(myUser.socketId).emit("newmess", data);
+    }
+  });
 
-// socketio.on("connection", (socket)=>{
-
-//     socket.on("addUser", (user)=>{
-       
-//         checkUser(user, socket.id)
-//         // console.log(users)
-//         socketio.emit("getUsers", users)
-//     })
- 
-//    socket.on("message", (data)=>{
-   
-//     const myUser = findUser(data.members[1])
-    
-//    if(myUser){
-//     socketio.to(myUser.socketId).emit("newmess", data)
-//    }
-    
-//    })
-
-//    socket.on("disconnect",()=>{
-    
-//     removeUser(socket.id)
-//     socketio.emit("leftUsers", users)
-//    })
-// }
-    
-//    )
+  socket.on("disconnect", () => {
+    removeUser(socket.id);
+    socketio.emit("leftUsers", users);
+  });
+});
